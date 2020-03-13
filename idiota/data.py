2aab78469dbd4cdf13b3e195b28d9519e4c27245
prakash <prakashsellathurai@gmail.com>
2020-03-13 13:03:19 +0530

    added hash object

--- idiota/data.py
+++ idiota/data.py
@@ -1,8 +1,16 @@
 import os
-
+import hashlib
 
 GIT_DIR = '.idiota'
 
 
 def init ():
     os.makedirs (GIT_DIR)
+    os.makedirs (f'{GIT_DIR}/objects')
+
+
+def hash_object (data):
+    oid = hashlib.sha1 (data).hexdigest ()
+    with open (f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write (data)
+    return oid
