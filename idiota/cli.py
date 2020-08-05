58aacca63331b3694fdeab3662430902a8a44e87
prakash <prakashsellathurai@gmail.com>
2020-08-05 13:41:39 +0530

    k-render-graph

--- idiota/cli.py
+++ idiota/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import subprocess
 import textwrap
 
 from . import base
@@ -104,13 +105,22 @@ def tag (args):
     base.create_tag (args.name, args.oid)
 
 def k (args):
+    dot = 'digraph commits {\n'
     oids = set ()
     for refname, ref in data.iter_refs ():
-        print (refname, ref)
+        dot += f'"{refname}" [shape=note]\n'
+        dot += f'"{refname}" -> "{ref}"\n'
         oids.add (ref)
 
     for oid in base.iter_commits_and_parents (oids):
         commit = base.get_commit (oid)
-        print (oid)
+        dot += f'"{oid}" [shape=box style=filled label="{oid[:10]}"]\n'
         if commit.parent:
-            print ('Parent', commit.parent)
+            dot += f'"{oid}" -> "{commit.parent}"\n'
+
+    dot += '}'
+    print (dot)
+        with subprocess.Popen (
+            ['dot', '-Tgtk', '/dev/stdin'],
+            stdin=subprocess.PIPE) as proc:
+        proc.communicate (dot.encode ())
