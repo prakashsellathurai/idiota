82fa8d5cd762d5ced2bb184ace2efc28c4cd8cf2
prakash <prakashsellathurai@gmail.com>
2020-08-21 14:49:32 +0530

    iterate-merge-head

--- idiota/data.py
+++ idiota/data.py
@@ -51,7 +51,7 @@ def _get_ref_internal (ref, deref):
     return ref, RefValue (symbolic=symbolic, value=value)
 
 def iter_refs (prefix='', deref=True):
-    refs = ['HEAD']
+    refs = ['HEAD', 'MERGE_HEAD']
     for root, _, filenames in os.walk (f'{GIT_DIR}/refs/'):
         root = os.path.relpath (root, GIT_DIR)
         refs.extend (f'{root}/{name}' for name in filenames)
@@ -59,7 +59,9 @@ def iter_refs (prefix='', deref=True):
     for refname in refs:
         if not refname.startswith (prefix):
             continue
-        yield refname, get_ref (refname, deref=deref)
+        ref = get_ref (refname, deref=deref)
+        if ref.value:
+            yield refname, ref
 
 def hash_object (data, type_='blob'):
     obj = type_.encode () + b'\x00' + data
