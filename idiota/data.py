e9bcb15a554a9cf7431315b70dac7a1697cbbfdb
prakash <prakashsellathurai@gmail.com>
2020-09-19 13:10:58 +0530

    updated type reading as blob

--- idiota/data.py
+++ idiota/data.py
@@ -9,12 +9,21 @@ def init ():
     os.makedirs (f'{GIT_DIR}/objects')
 
 
-def hash_object (data):
-    oid = hashlib.sha1 (data).hexdigest ()
+def hash_object (data, type_='blob'):
+    obj = type_.encode () + b'\x00' + data
+    oid = hashlib.sha1 (obj).hexdigest ()
     with open (f'{GIT_DIR}/objects/{oid}', 'wb') as out:
-        out.write (data)
+        out.write (obj)
     return oid
 
-def get_object (oid):
+def get_object (oid, expected='blob'):
     with open (f'{GIT_DIR}/objects/{oid}', 'rb') as f:
-        return f.read ()
+        obj = f.read ()
+
+    first_null = obj.index (b'\x00')
+    type_ = obj[:first_null].decode ()
+    content = obj[first_null + 1:]
+
+    if expected is not None:
+        assert type_ == expected, f'Expected {expected}, got {type_}'
+    return content
