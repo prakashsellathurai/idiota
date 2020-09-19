38e26468ec5aa60446f475174978aa8fb4059399
prakash <prakashsellathurai@gmail.com>
2020-09-19 13:54:46 +0530

    create-refvalue

--- idiota/base.py
+++ idiota/base.py
@@ -71,7 +71,9 @@ def read_tree (tree_oid):
 
 def commit (message):
     commit = f'tree {write_tree ()}\n'
-    HEAD = data.get_ref ('HEAD')
+
+    HEAD = data.get_ref ('HEAD').value
+
     if HEAD:
         commit += f'parent {HEAD}\n'
 
@@ -80,20 +82,20 @@ def commit (message):
 
     oid = data.hash_object (commit.encode (), 'commit')
 
-    data.update_ref ('HEAD', oid)
-
+    data.update_ref ('HEAD', data.RefValue (symbolic=False, value=oid))
+    
     return oid
 
 def checkout (oid):
     commit = get_commit (oid)
     read_tree (commit.tree)
-    data.update_ref ('HEAD', oid)
+    data.update_ref ('HEAD', data.RefValue (symbolic=False, value=oid))
 
 def create_tag (name, oid):
-    data.update_ref (f'refs/tags/{name}', oid)
+    data.update_ref (f'refs/tags/{name}', data.RefValue (symbolic=False, value=oid))
 
 def create_branch (name, oid):
-    data.update_ref (f'refs/heads/{name}', oid)
+    data.update_ref (f'refs/heads/{name}', data.RefValue (symbolic=False, value=oid))
 
 Commit = namedtuple ('Commit', ['tree', 'parent', 'message'])
 
@@ -140,8 +142,8 @@ def get_oid (name):
         f'refs/heads/{name}',
     ]
     for ref in refs_to_try:
-        if data.get_ref (ref):
-            return data.get_ref (ref)
+        if data.get_ref (ref).value:
+            return data.get_ref (ref).value
 
     # Name is SHA1
     is_hex = all (c in string.hexdigits for c in name)
