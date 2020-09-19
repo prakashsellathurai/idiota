540f34a0906642bea66ddcbd7765ae7e754e525f
prakash <prakashsellathurai@gmail.com>
2020-09-19 13:26:50 +0530

    checkout-read-tree

--- idiota/base.py
+++ idiota/base.py
@@ -83,6 +83,11 @@ def commit (message):
     data.set_HEAD (oid)
 
     return oid
+    
+def checkout (oid):
+    commit = get_commit (oid)
+    read_tree (commit.tree)
+    data.set_HEAD (oid)
 
 Commit = namedtuple ('Commit', ['tree', 'parent', 'message'])
 
--- idiota/cli.py
+++ idiota/cli.py
@@ -40,6 +40,11 @@ def parse_args ():
 
     log_parser = commands.add_parser ('log')
     log_parser.set_defaults (func=log)
+    log_parser.add_argument ('oid', nargs='?')
+
+    checkout_parser = commands.add_parser ('checkout')
+    checkout_parser.set_defaults (func=checkout)
+    checkout_parser.add_argument ('oid')
 
     return parser.parse_args ()
 
@@ -71,7 +76,7 @@ def commit (args):
     print (base.commit (args.message))\
 
 def log (args):
-    oid = data.get_HEAD ()
+    oid = args.oid or data.get_HEAD ()
     while oid:
         commit = base.get_commit (oid)
 
@@ -80,3 +85,6 @@ def log (args):
         print ('')
 
         oid = commit.parent
+
+def checkout (args):
+    base.checkout (args.oid)
