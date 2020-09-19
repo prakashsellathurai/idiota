0f12b1382f8a8360737fe5df768167984ecf2b36
prakash <prakashsellathurai@gmail.com>
2020-09-19 13:34:43 +0530

    search-ref-directories

--- idiota/base.py
+++ idiota/base.py
@@ -1,7 +1,7 @@
 import itertools
 import operator
 import os
-
+import string
 from collections import namedtuple
 
 from . import data
@@ -113,7 +113,23 @@ def get_commit (oid):
     return Commit (tree=tree, parent=parent, message=message)
 
 def get_oid (name):
-    return data.get_ref (name) or name
+    # Name is ref
+    refs_to_try = [
+        f'{name}',
+        f'refs/{name}',
+        f'refs/tags/{name}',
+        f'refs/heads/{name}',
+    ]
+    for ref in refs_to_try:
+        if data.get_ref (ref):
+            return data.get_ref (ref)
+
+    # Name is SHA1
+    is_hex = all (c in string.hexdigits for c in name)
+    if len (name) == 40 and is_hex:
+        return name
+
+    assert False, f'Unknown name {name}'
 
 def is_ignored (path):
     return '.idiota' in path.split ('/')
