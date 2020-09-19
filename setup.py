21805da80908e6f334c8a6f4c6d2ed06a3402c82
prakash <prakashsellathurai@gmail.com>
2020-09-19 16:30:54 +0530

    updated readme

--- setup.py
+++ setup.py
@@ -2,11 +2,25 @@
 
 from setuptools import setup
 
-setup (name = 'idiota',
-       version = '1.0',
-       packages = ['idiota'],
-       entry_points = {
-           'console_scripts' : [
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='idiota',
+      version='2.1',
+      packages=['idiota'],
+      entry_points={
+           'console_scripts': [
                'idiota = idiota.cli:main'
            ]
-       })
+      },
+
+
+      author='Prakash Sellathurai',
+      author_email='prakashsellathurai@gmail.com',
+      description='idiota is a minimal version control system built on python',
+      long_description=long_description,
+      long_description_content_type='text/markdown',  # This is important!
+
+      url='https://github.com/prakashsellathurai/idiota',
+
+      )
